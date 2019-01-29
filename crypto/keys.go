f27055e8e9c559738835907705c00e46cc1d24df
shzhsh <43815636+shzhsh@users.noreply.github.com>
2019-01-29 13:01:58 +0000

    fix error when missing pubkey
    ed25519.PrivateKey includes the PublicKey in the latter 32 bytes, if it's missing (when importing a private key) we need to make a new PrivateKey using the old key as the seed

--- crypto/keys.go
+++ crypto/keys.go
@@ -1,13 +1,15 @@
 package crypto
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 
 	"golang.org/x/crypto/ed25519"
 )
 
 type PublicKey [32]byte
-type PrivateKey [64]byte
+type PrivateKey [64]byte // PrivateKey + PublicKey
 
 type Signature [64]byte
 
@@ -16,6 +18,20 @@ func GenPrivKey() PrivateKey {
 	edPrivKey := ed25519.NewKeyFromSeed(RandBytes(32))
 	copy(privKey[:], edPrivKey)
 	return privKey
+}
+
+// Set the PublicKey in PrivateKey if missing
+func (privKey *PrivateKey) SetPubKey() {
+	privKeyBytes := privKey[:]
+
+	for _, v := range privKeyBytes[32:] {
+		if v != 0 {
+			return
+		}
+	}
+
+	edPrivKey := ed25519.NewKeyFromSeed(privKey[:32])
+	copy(privKey[:], edPrivKey[:])
 }
 
 func (privKey PrivateKey) PubKey() PublicKey {
@@ -29,8 +45,8 @@ func (privKey PrivateKey) PubKey() PublicKey {
 		}
 	}
 
-	pubKeyBytes := ed25519.PrivateKey(privKeyBytes).Public().([]byte)
-	copy(pubKey[:], pubKeyBytes)
+	edPrivKey := ed25519.NewKeyFromSeed(privKey[:32])
+	copy(pubKey[:], edPrivKey[32:])
 	return pubKey
 }
 
@@ -59,4 +75,69 @@ func (pubKey PublicKey) StringWithDashes() string {
 func (pubKey PublicKey) StringCompact() string {
 	x := fmt.Sprintf("%x", pubKey[:])
 	return fmt.Sprintf("%v...%v", x[:4], x[60:])
+}
+
+func (privKey PrivateKey) String() string {
+	return fmt.Sprintf("%x", privKey[:32])
+}
+
+func (sig Signature) String() string {
+	return fmt.Sprintf("%x", sig[:])
+}
+
+func (pubKey PublicKey) MarshalJSON() ([]byte, error) {
+	b, err := json.Marshal(pubKey.String())
+	return b, err
+}
+
+func (privKey PrivateKey) MarshalJSON() ([]byte, error) {
+	b, err := json.Marshal(privKey.String())
+	return b, err
+}
+
+func (sig Signature) MarshalJSON() ([]byte, error) {
+	b, err := json.Marshal(sig.String())
+	return b, err
+}
+
+func (pubKey PublicKey) UnmarshalJSON(data []byte) error {
+	var x string
+	err := json.Unmarshal(data, x)
+	if err != nil {
+		return err
+	}
+	b, err := hex.DecodeString(x)
+	if err != nil {
+		return err
+	}
+	copy(pubKey[:], b)
+	return nil
+}
+
+func (privKey PrivateKey) UnmarshalJSON(data []byte) error {
+	var x string
+	err := json.Unmarshal(data, x)
+	if err != nil {
+		return err
+	}
+	b, err := hex.DecodeString(x)
+	if err != nil {
+		return err
+	}
+	copy(privKey[:], b)
+	return nil
+}
+
+func (sig Signature) UnmarshalJSON(data []byte) error {
+	var x string
+	err := json.Unmarshal(data, x)
+	if err != nil {
+		return err
+	}
+	b, err := hex.DecodeString(x)
+	if err != nil {
+		return err
+	}
+	copy(sig[:], b)
+	return nil
 }
--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -1,13 +1,26 @@
 package crypto
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestSigVerify(t *testing.T) {
 	privKey := GenPrivKey()
 	pubKey := privKey.PubKey()
-	msg := RandBytes(257)
+	msg := RandBytes(64)
 	sig := privKey.Sign(msg)
 	if valid := pubKey.Verify(msg, sig); !valid {
 		t.Error("invalid signature")
+	}
+}
+
+func TestPubKey(t *testing.T) {
+	privKey := GenPrivKey()
+	newPrivKeyP := new(PrivateKey)
+	copy(newPrivKeyP[:], privKey[:32])
+	newPrivKeyP.SetPubKey()
+	if bytes.Compare(privKey[32:], newPrivKeyP[32:]) != 0 {
+		t.Error("new PublicKey doesn't match")
 	}
 }
